e0990aa25c2798f731e2ce0a4289599d855460dd
e-kolkhoz <e-kolkhoz@ya.ru>
2021-08-06 10:31:50 +0300

    upd

---
+++ upd_abi_and_addrs.js
@@ -0,0 +1,40 @@
+fs = require('fs');
+
+const CONTRACTS_PROJ_PTH = '../Eurity' //place your eurity contracts project path here
+const ADRESSES_FN = 'localForkDeploymentOutput.json'
+
+const LIB_ETHERS_PTH = './packages/lib-ethers'
+const MAINNET_CFG_FN = '/deployments/default/mainnet.json'
+
+
+
+var actual_data_js = fs.readFileSync(CONTRACTS_PROJ_PTH+'/mainnetDeployment/' +ADRESSES_FN, 'utf8');
+var actual_data = JSON.parse(actual_data_js);
+
+var mainnet_cfg_js = fs.readFileSync(LIB_ETHERS_PTH+MAINNET_CFG_FN, 'utf8');
+var mainnet_cfg = JSON.parse(mainnet_cfg_js);
+
+
+for(var contract_name in mainnet_cfg.addresses){
+    console.log(contract_name)
+    mainnet_cfg.addresses[contract_name] = actual_data[contract_name].address;
+    console.log(mainnet_cfg.addresses[contract_name]);
+}
+fs.writeFileSync( LIB_ETHERS_PTH+MAINNET_CFG_FN, JSON.stringify(mainnet_cfg, null, 2))
+
+const name2abi = {}
+for (contract_dir of fs.readdirSync(CONTRACTS_PROJ_PTH+'/artifacts/contracts')){
+  if (!contract_dir.includes('.sol')){continue}
+  console.log(contract_dir)
+  var name = contract_dir.split('.')[0]
+  full_pth = CONTRACTS_PROJ_PTH+'/artifacts/contracts/'+contract_dir+'/'+name+'.json'
+  console.log(full_pth)
+  var contract_artefact = fs.readFileSync(full_pth, 'utf8')
+  name2abi[name] = JSON.parse(contract_artefact)['abi']
+}
+
+for (name in name2abi){
+  console.log(name)
+  fs.writeFileSync( LIB_ETHERS_PTH+'/abi/'+name+'.json', JSON.stringify(name2abi[name], null, 2))
+
+}
